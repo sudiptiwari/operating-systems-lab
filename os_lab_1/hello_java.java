460226891f7763b0c3dec9786a869ca7df00ac7c
Sudip Tiwari <sudip10063@gmail.com>
2023-12-14 18:45:36 +0545

    add os lab 1

---
+++ os_lab_1/hello_java.java
@@ -0,0 +1,7 @@
+public class hello_java {
+    public static void main(String[] args) {
+        // Print Hello, World! to the console
+        System.out.println("Hello, World from Java!");
+    }
+}
+
